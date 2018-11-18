5ac3731c0e0556ff5d2f02ac70fbd9564fd38abf
Daniël Illouz <daniel.illouz@me.com>
2018-11-18 21:57:49 +0100

    Implement rectangle Area func

--- code/structs/rectangle_test.go
+++ code/structs/rectangle_test.go
@@ -23,5 +23,26 @@ func BenchmarkPerimeter(b *testing.B) {
 func ExamplePerimeter() {
 	perimeter := Perimeter(7.0, 4.0)
 	fmt.Println(perimeter)
-	// Output: 22.2
+	// Output: 22
+}
+
+func TestArea(t *testing.T) {
+	got := Area(5.0, 5.0)
+	want := 25.0
+
+	if got != want {
+		t.Errorf("Got %.2f, want %.2f", got, want)
+	}
+}
+
+func BenchmarkArea(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Area(11.0, 56.0)
+	}
+}
+
+func ExampleArea() {
+	area := Area(4.0, 4.0)
+	fmt.Println(area)
+	// Output: 16
 }
