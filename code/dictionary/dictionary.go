1b197ebbce27c68180526175669a43c4568cac1f
Daniël Illouz <daniel.illouz@me.com>
2018-11-20 00:59:36 +0100

    Create custom Dictionary type and handle errors

--- code/dictionary/dictionary.go
+++ code/dictionary/dictionary.go
@@ -1,6 +1,22 @@
 package dictionary
 
-// Search returns a value for a specific key in a dictionary.
-func Search(dict map[string]int, key string) int {
-	return dict[key]
+import (
+	"errors"
+)
+
+// ErrorNotFound is given when a key in a Dictionary is not found
+var ErrorNotFound = errors.New("key not found")
+
+// Dictionary holds keys that represents integers.
+type Dictionary map[string]int
+
+// Search returns a value for a specific key in Dictionary.
+func (d Dictionary) Search(key string) (int, error) {
+	val, ok := d[key]
+
+	if !ok {
+		return val, ErrorNotFound
+	}
+
+	return val, nil
 }
--- code/dictionary/dictionary_test.go
+++ code/dictionary/dictionary_test.go
@@ -5,18 +5,36 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dict := map[string]int{
+	dict := Dictionary{
 		"one":   1,
 		"two":   2,
 		"three": 3,
 		"four":  4,
 	}
 
-	key := "two"
-	got := Search(dict, key)
-	want := dict[key]
+	assertError := func(t *testing.T, got, want error) {
+		if got == nil {
+			t.Fatalf("got %v, want err", got)
+		}
 
-	if got != want {
-		t.Errorf("got %v, want %v, for dict %#+v and key %s", got, want, dict, key)
+		if got != want {
+			t.Errorf("got err %v, want err %v", got, want)
+		}
 	}
+
+	t.Run("match", func(t *testing.T) {
+		key := "two"
+		got, _ := dict.Search(key)
+		want := dict[key]
+
+		if got != want {
+			t.Errorf("got %v, want %v, for dict %#+v and key %s", got, want, dict, key)
+		}
+	})
+
+	t.Run("no match", func(t *testing.T) {
+		_, err := dict.Search("nokey")
+
+		assertError(t, err, ErrorNotFound)
+	})
 }
